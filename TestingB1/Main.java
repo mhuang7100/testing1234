15cf825ae775c2c77d72e970fe745eec4e741789
mhuang7100 <mhuang00@ladueschools.net>
2022-10-13 13:20:25 +0000

    poop

--- TestingB1/Main.java
+++ TestingB1/Main.java
@@ -1,5 +1,9 @@
+package TestingB1;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Sayam is pretty cool");
+        poop poopee = new poop();
+        
     }
 }
