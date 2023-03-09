3d4bd989abf6ac8b7b15132b5c78502b93a9f19e
mhuang7100 <mhuang00@ladueschools.net>
2023-03-09 13:37:39 +0000

    inheritance mini proj

--- Inheritance/Classroom.java
+++ Inheritance/Classroom.java
@@ -10,4 +10,12 @@ public class Classroom extends Room{
     public int calculateNumDesks(){
         return numPeople();
     }
+
+    public void setSubject(String sub){
+        subject = sub;
+    }
+
+    public String getSubject(){
+        return subject;
+    }
 }
